c7b9c72e5f369711ef10b3bbf699d74048edd363
Andreushk <andrewshkt@gmail.com>
2024-07-18 10:45:43 +0300

    bugfix: make the useDisableBodyScoll hook more readable

--- src/hooks/useDisableBodyScroll.ts
+++ src/hooks/useDisableBodyScroll.ts
@@ -2,11 +2,9 @@ import { useEffect } from 'react';
 
 const useDisableBodyScroll = (isDisabled: boolean): void => {
   useEffect(() => {
-    if (isDisabled) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'unset';
-    }
+    isDisabled
+      ? (document.body.style.overflow = 'hidden')
+      : (document.body.style.overflow = 'unset');
 
     return () => {
       document.body.style.overflow = 'unset';
